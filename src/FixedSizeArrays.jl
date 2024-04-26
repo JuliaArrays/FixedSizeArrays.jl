6f067e6d84c4ee9860c9576757419456e66bba65
Neven Sajko <s@purelymail.com>
2024-04-26 05:15:05 +0200

    fix `checked_dims`
    xref https://github.com/JuliaLang/julia/issues/54244
    
    Fixes #40

--- src/FixedSizeArrays.jl
+++ src/FixedSizeArrays.jl
@@ -47,29 +47,32 @@ Base.isassigned(a::FixedSizeArray, i::Int) = isassigned(a.mem, i)
 
 # safe product of a tuple of integers, for calculating dimensions size
 
-checked_dims_impl(a::Int, ::Tuple{}) = a
-function checked_dims_impl(a::Int, t::Tuple{Int,Vararg{Int,N}}) where {N}
+checked_dims_impl(a::Int, ::Tuple{}, have_overflow::Bool) = (a, have_overflow)
+function checked_dims_impl(a::Int, t::Tuple{Int,Vararg{Int,N}}, have_overflow::Bool) where {N}
     b = first(t)
-    tmax = typemax(a)
-    if (a == tmax) || (b == tmax)
-        throw(ArgumentError("array dimension size can't be the maximum representable value"))
-    end
-    if (a < 0) || (b < 0)
-        throw(ArgumentError("array dimension size can't be negative"))
-    end
     (m, o) = Base.Checked.mul_with_overflow(a, b)
-    if o
-        throw(ArgumentError("array dimensions too great, can't represent length"))
-    end
     r = Base.tail(t)::NTuple{N,Int}
-    checked_dims_impl(m, r)::Int
+    checked_dims_impl(m, r, have_overflow | o)::Tuple{Int,Bool}
 end
 
 checked_dims(::Tuple{}) = 1
 function checked_dims(t::Tuple{Int,Vararg{Int,N}}) where {N}
+    any_is_zero     = any(iszero,                 t)::Bool
+    any_is_negative = any((x -> x < false),       t)::Bool
+    any_is_typemax  = any((x -> x == typemax(x)), t)::Bool
     a = first(t)
     r = Base.tail(t)::NTuple{N,Int}
-    checked_dims_impl(a, r)::Int
+    (product, have_overflow) = checked_dims_impl(a, r, false)::Tuple{Int,Bool}
+    if any_is_negative
+        throw(ArgumentError("array dimension size can't be negative"))
+    end
+    if any_is_typemax
+        throw(ArgumentError("array dimension size can't be the maximum representable value"))
+    end
+    if have_overflow & !any_is_zero
+        throw(ArgumentError("array dimensions too great, can't represent length"))
+    end
+    product
 end
 
 # broadcasting
