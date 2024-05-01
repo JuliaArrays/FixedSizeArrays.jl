119503aa58c5f2444710d3f8b217b5b8c4589c4e
Neven Sajko <s@purelymail.com>
2024-05-01 19:48:30 +0200

    fix tests 1

--- test/runtests.jl
+++ test/runtests.jl
@@ -68,11 +68,17 @@ end
         end
     end
 
+    storage_types = (Memory, Vector, fsv(Memory), fsv(Vector), fsv(fsv(Memory)))
+    storage_type = first(storage_types)
+    FSV = fsv(storage_type)
+    FSM = fsm(storage_type)
+    FSA = fsa(storage_type)
+
     @testset "Constructors" begin
         for dim_count ∈ 0:4
             siz = ntuple(Returns(2), dim_count)
-            T = FixedSizeArray{Float64}
-            R = FixedSizeArray{Float64,dim_count}
+            T = FSA{Float64}
+            R = FSA{Float64,dim_count}
             for sz ∈ (siz, map(BigInt, siz))
                 for args ∈ ((undef, sz), (undef, sz...))
                     test_inferred(   R, args)
@@ -83,34 +89,34 @@ end
         for offset ∈ (-1, 0, 2, 3)
             ax = offset:(offset + 1)
             oa = OffsetArray([10, 20], ax)
-            @test_throws DimensionMismatch FixedSizeArray(oa)
-            @test_throws DimensionMismatch FixedSizeVector(oa)
-            @test_throws DimensionMismatch FixedSizeArray{Int}(oa)
-            @test_throws DimensionMismatch FixedSizeVector{Int}(oa)
+            @test_throws DimensionMismatch FSA(oa)
+            @test_throws DimensionMismatch FSV(oa)
+            @test_throws DimensionMismatch FSA{Int}(oa)
+            @test_throws DimensionMismatch FSV{Int}(oa)
         end
         @testset "taken from Julia's test/core.jl" begin
             # inspired by:
             # https://github.com/JuliaLang/julia/blob/83929ad883c97fa4376618c4559b74f6ada7a3ce/test/core.jl#L7211-L7228
             b = prevpow(2, typemax(Int))
-            test_inferred(FixedSizeArray{Int}, FixedSizeArray{Int,3}, (undef, 0, b, b))
-            test_inferred(FixedSizeArray{Int}, FixedSizeArray{Int,3}, (undef, b, b, 0))
-            test_inferred(FixedSizeArray{Int}, FixedSizeArray{Int,5}, (undef, b, b, 0, b, b))
-            @test_throws ArgumentError FixedSizeArray{Int}(undef, b, b)
-            @test_throws ArgumentError FixedSizeArray{Int}(undef, 1, b, b)
-            @test_throws ArgumentError FixedSizeArray{Int}(undef, 0, -10)
-            @test_throws ArgumentError FixedSizeArray{Int}(undef, -10, 0)
-            @test_throws ArgumentError FixedSizeArray{Int}(undef, -1, -1)
-            @test_throws ArgumentError FixedSizeArray{Int}(undef, 0, -4, -4)
-            @test_throws ArgumentError FixedSizeArray{Int}(undef, -4, 1, 0)
-            @test_throws ArgumentError FixedSizeArray{Int}(undef, -4, -4, 1)
+            test_inferred(FSA{Int}, FSA{Int,3}, (undef, 0, b, b))
+            test_inferred(FSA{Int}, FSA{Int,3}, (undef, b, b, 0))
+            test_inferred(FSA{Int}, FSA{Int,5}, (undef, b, b, 0, b, b))
+            @test_throws ArgumentError FSA{Int}(undef, b, b)
+            @test_throws ArgumentError FSA{Int}(undef, 1, b, b)
+            @test_throws ArgumentError FSA{Int}(undef, 0, -10)
+            @test_throws ArgumentError FSA{Int}(undef, -10, 0)
+            @test_throws ArgumentError FSA{Int}(undef, -1, -1)
+            @test_throws ArgumentError FSA{Int}(undef, 0, -4, -4)
+            @test_throws ArgumentError FSA{Int}(undef, -4, 1, 0)
+            @test_throws ArgumentError FSA{Int}(undef, -4, -4, 1)
         end
-        @test_throws ArgumentError FixedSizeArray{Float64,1}(undef, -1)
-        @test_throws ArgumentError FixedSizeArray{Float64,1}(undef, (-1,))
-        @test_throws ArgumentError FixedSizeArray{Float64,2}(undef, -1, -1)
-        @test_throws ArgumentError FixedSizeArray{Float64,2}(undef, typemax(Int), 0)
-        @test_throws ArgumentError FixedSizeArray{Float64,2}(undef, typemax(Int), typemax(Int))
-        @test_throws ArgumentError FixedSizeArray{Float64,3}(undef, typemax(Int)-1, typemax(Int)-1, 2)
-        @test_throws ArgumentError FixedSizeArray{Float64,4}(undef, typemax(Int)-1, typemax(Int)-1, 2, 4)
+        @test_throws ArgumentError FSA{Float64,1}(undef, -1)
+        @test_throws ArgumentError FSA{Float64,1}(undef, (-1,))
+        @test_throws ArgumentError FSA{Float64,2}(undef, -1, -1)
+        @test_throws ArgumentError FSA{Float64,2}(undef, typemax(Int), 0)
+        @test_throws ArgumentError FSA{Float64,2}(undef, typemax(Int), typemax(Int))
+        @test_throws ArgumentError FSA{Float64,3}(undef, typemax(Int)-1, typemax(Int)-1, 2)
+        @test_throws ArgumentError FSA{Float64,4}(undef, typemax(Int)-1, typemax(Int)-1, 2, 4)
         @testset "negative dimension size" begin
             for n ∈ 2:4
                 funs = (
@@ -123,38 +129,38 @@ end
                 fun = f -> ntuple(f, n)
                 sizes = map(fun, funs)
                 for siz ∈ sizes
-                    @test_throws ArgumentError FixedSizeArray{Float64,n}(undef, siz)
-                    @test_throws ArgumentError FixedSizeArray{Float64,n}(undef, siz...)
+                    @test_throws ArgumentError FSA{Float64,n}(undef, siz)
+                    @test_throws ArgumentError FSA{Float64,n}(undef, siz...)
                 end
             end
         end
     end
 
     @testset "FixedSizeVector" begin
-        v = FixedSizeVector{Float64}(undef, 3)
+        v = FSV{Float64}(undef, 3)
         @test length(v) == 3
-        test_inferred_noalloc(((a, b) -> a .= b), FixedSizeVector{Float64}, (v, 1:3))
+        test_inferred_noalloc(((a, b) -> a .= b), FSV{Float64}, (v, 1:3))
         @test v == 1:3
-        test_inferred(similar, FixedSizeVector{Float64}, (v,))
-        test_inferred(similar, FixedSizeVector{Int}, (v, Int))
-        test_inferred(copy, FixedSizeVector{Float64}, (v,))
-        test_inferred(zero, FixedSizeVector{Float64}, (v,))
-        test_inferred(similar, FixedSizeVector{Int}, (FixedSizeVector{Int}, (2,)))
-        test_inferred(similar, FixedSizeVector{Int}, (FixedSizeArray{Int}, (2,)))
+        test_inferred(similar, FSV{Float64}, (v,))
+        test_inferred(similar, FSV{Int}, (v, Int))
+        test_inferred(copy, FSV{Float64}, (v,))
+        test_inferred(zero, FSV{Float64}, (v,))
+        test_inferred(similar, FSV{Int}, (FSV{Int}, (2,)))
+        test_inferred(similar, FSV{Int}, (FSA{Int}, (2,)))
         example_abstract_vectors = (7:9, [7, 8, 9], OffsetArray([7, 8, 9], 1:3))
         test_convert = function(T, a)
-            test_inferred(convert, FixedSizeVector{Int}, (T, a))
+            test_inferred(convert, FSV{Int}, (T, a))
             c = convert(T, a)
-            test_inferred(convert, FixedSizeVector{Int}, (T, c))
+            test_inferred(convert, FSV{Int}, (T, c))
             @test c == a
             @test c === convert(T, c)
         end
-        for T ∈ (FixedSizeArray, FixedSizeVector)
+        for T ∈ (FSA, FSV)
             for a ∈ example_abstract_vectors
                 test_convert(T, a)
             end
         end
-        for T ∈ (FixedSizeArray{Int}, FixedSizeVector{Int})
+        for T ∈ (FSA{Int}, FSV{Int})
             for S ∈ (Int, Float64)
                 for a ∈ map((c -> map(S, c)), example_abstract_vectors)
                     test_convert(T, a)
@@ -164,34 +170,34 @@ end
     end
 
     @testset "FixedSizeMatrix" begin
-        m = FixedSizeMatrix{Float64}(undef, 3, 3)
+        m = FSM{Float64}(undef, 3, 3)
         m_ref = reshape(1:9, size(m))
         @test length(m) == 9
-        test_inferred_noalloc(((a, b) -> a .= b), FixedSizeMatrix{Float64}, (m, m_ref))
+        test_inferred_noalloc(((a, b) -> a .= b), FSM{Float64}, (m, m_ref))
         @test m == m_ref
-        test_inferred(similar, FixedSizeMatrix{Float64}, (m,))
-        test_inferred(similar, FixedSizeMatrix{Int}, (m, Int))
-        test_inferred(copy, FixedSizeMatrix{Float64}, (m,))
-        test_inferred(zero, FixedSizeMatrix{Float64}, (m,))
-        test_inferred(similar, FixedSizeMatrix{Int}, (FixedSizeMatrix{Int}, (2, 3)))
-        test_inferred(similar, FixedSizeMatrix{Int}, (FixedSizeArray{Int}, (2, 3)))
+        test_inferred(similar, FSM{Float64}, (m,))
+        test_inferred(similar, FSM{Int}, (m, Int))
+        test_inferred(copy, FSM{Float64}, (m,))
+        test_inferred(zero, FSM{Float64}, (m,))
+        test_inferred(similar, FSM{Int}, (FSM{Int}, (2, 3)))
+        test_inferred(similar, FSM{Int}, (FSA{Int}, (2, 3)))
         example_abstract_matrices = (
             reshape(1:9, (3, 3)),
             OffsetArray(reshape(1:9, (3, 3)), 1:3, 1:3),
         )
         test_convert = function(T, a)
-            test_inferred(convert, FixedSizeMatrix{Int}, (T, a))
+            test_inferred(convert, FSM{Int}, (T, a))
             c = convert(T, a)
-            test_inferred(convert, FixedSizeMatrix{Int}, (T, c))
+            test_inferred(convert, FSM{Int}, (T, c))
             @test c == a
             @test c === convert(T, c)
         end
-        for T ∈ (FixedSizeArray, FixedSizeMatrix)
+        for T ∈ (FSA, FSM)
             for a ∈ example_abstract_matrices
                 test_convert(T, a)
             end
         end
-        for T ∈ (FixedSizeArray{Int}, FixedSizeMatrix{Int})
+        for T ∈ (FSA{Int}, FSM{Int})
             for S ∈ (Int, Float64)
                 for a ∈ map((c -> map(S, c)), example_abstract_matrices)
                     test_convert(T, a)
@@ -203,18 +209,18 @@ end
     @testset "`map`" begin
         for dim_count ∈ 0:3
             size = ntuple(Returns(3), dim_count)
-            a = FixedSizeArray{Int, dim_count}(undef, size)
+            a = FSA{Int, dim_count}(undef, size)
             for v ∈ (3, 3.1, nothing)
                 args = (Returns(v), a)
                 @test all(==(v), map(args...))
-                test_inferred(map, FixedSizeArray{typeof(v), dim_count}, args)
+                test_inferred(map, FSA{typeof(v), dim_count}, args)
             end
         end
     end
 
     @testset "broadcasting" begin
-        TVI = FixedSizeVector{Int}
-        TVF = FixedSizeVector{Float64}
+        TVI = FSV{Int}
+        TVF = FSV{Float64}
         v3 = TVI(1:3)
         vf3 = TVF(1:3)
         bp = (x, y) -> x .+ y
@@ -230,8 +236,8 @@ end
         test_inferred(bp, TVF, (v3, .3))
         test_inferred(bm, TVF, (v3, .3))
         @testset "matrices" begin
-            TMI = FixedSizeMatrix{Int}
-            TMF = FixedSizeMatrix{Float64}
+            TMI = FSM{Int}
+            TMF = FSM{Float64}
             m33 = TMI(undef, 3, 3)
             m13 = TMI(undef, 1, 3)
             m31 = TMI(undef, 3, 1)
@@ -255,9 +261,9 @@ end
 
     @testset "`isassigned`" begin
         for dim_count ∈ 0:3
-            for elem_type ∈ (Int, FixedSizeMatrix{Nothing})
+            for elem_type ∈ (Int, FSM{Nothing})
                 size = ntuple(Returns(3), dim_count)
-                a = FixedSizeArray{elem_type, dim_count}(undef, size)
+                a = FSA{elem_type, dim_count}(undef, size)
                 for i_style ∈ (IndexLinear(), IndexCartesian())
                     for i ∈ eachindex(i_style, a)
                         @test isassigned(a, i) == isbitstype(elem_type)
@@ -267,10 +273,10 @@ end
             end
         end
         @testset "some assigned" begin
-            a = FixedSizeMatrix{FixedSizeMatrix{Nothing}}(undef, 3, 3)
+            a = FSM{FSM{Nothing}}(undef, 3, 3)
             assigned_inds = ((1, 2), (2, 3), (3, 3))
             for ij ∈ assigned_inds
-                a[ij...] = FixedSizeMatrix{Nothing}(undef, 1, 1)
+                a[ij...] = FSM{Nothing}(undef, 1, 1)
             end
             for ij ∈ Iterators.product(1:3, 1:3)
                 @test isassigned(a, ij...) == (ij ∈ assigned_inds)
@@ -281,11 +287,11 @@ end
 
     @testset "`copyto!`" begin
         for (D, S) ∈ (
-            (Vector, FixedSizeVector),
-            (Memory, FixedSizeVector),
-            (FixedSizeVector, Vector),
-            (FixedSizeVector, Memory),
-            (FixedSizeVector, FixedSizeVector),
+            (Vector, FSV),
+            (Memory, FSV),
+            (FSV, Vector),
+            (FSV, Memory),
+            (FSV, FSV),
         )
             for E ∈ (Float64, Int)
                 s = S{E}(undef, 5)
@@ -304,20 +310,20 @@ end
     @testset "LinearAlgebra" begin
         @testset "$T" for T in (Float16, Float32, Float64)
             v = randn(T, 8)
-            v_fixed = FixedSizeVector(v)
-            test_inferred(+, FixedSizeVector{T}, (v_fixed, v_fixed))
+            v_fixed = FSV(v)
+            test_inferred(+, FSV{T}, (v_fixed, v_fixed))
             v_sum = v_fixed + v_fixed
             @test v_sum ≈ v + v
-            test_inferred(((x, y) -> x * y'), FixedSizeMatrix{T}, (v_fixed, v_fixed))
+            test_inferred(((x, y) -> x * y'), FSM{T}, (v_fixed, v_fixed))
             v_mul = v_fixed * v_fixed'
             @test v_mul ≈ v * v'
 
             M = randn(T, 8, 8)
-            M_fixed = FixedSizeMatrix(M)
-            test_inferred(+, FixedSizeMatrix{T}, (M_fixed, M_fixed))
+            M_fixed = FSM(M)
+            test_inferred(+, FSM{T}, (M_fixed, M_fixed))
             M_sum = M_fixed + M_fixed
             @test M_sum ≈ M + M
-            test_inferred(*, FixedSizeMatrix{T}, (M_fixed, M_fixed))
+            test_inferred(*, FSM{T}, (M_fixed, M_fixed))
             M_mul = M_fixed * M_fixed
             @test M_mul ≈ M * M
         end
@@ -327,7 +333,7 @@ end
         for elem_type ∈ (Int8, Int16, Int32, Int64)
             for dim_count ∈ 0:4
                 siz = ntuple(Returns(2), dim_count)
-                arr = FixedSizeArray{elem_type, dim_count}(undef, siz)
+                arr = FSA{elem_type, dim_count}(undef, siz)
                 @test pointer(arr) === pointer(arr, 1) === pointer(arr, 2) - sizeof(elem_type)
                 test_inferred_noalloc(pointer, Ptr{elem_type}, (arr,))
                 test_inferred_noalloc(pointer, Ptr{elem_type}, (arr, 1))
@@ -348,13 +354,13 @@ end
             for len ∈ keys(length_to_shapes)
                 shapes = length_to_shapes[len]
                 for shape1 ∈ shapes
-                    a = FixedSizeArray{elem_type,length(shape1)}(undef, shape1)
+                    a = FSA{elem_type,length(shape1)}(undef, shape1)
                     @test_throws DimensionMismatch reshape(a, length(a)+1)
                     @test_throws DimensionMismatch reshape(a, length(a)+1, 1)
                     @test_throws DimensionMismatch reshape(a, 1, length(a)+1)
                     for shape2 ∈ shapes
                         @test prod(shape1) === prod(shape2) === len  # meta
-                        T = FixedSizeArray{elem_type,length(shape2)}
+                        T = FSA{elem_type,length(shape2)}
                         test_inferred_noalloc(reshape, T, (a, shape2))
                         test_inferred_noalloc(reshape, T, (a, shape2...))
                         b = reshape(a, shape2)
@@ -368,16 +374,16 @@ end
     end
 
     @testset "`collect_as`" begin
-        for T ∈ (FixedSizeArray, FixedSizeVector, FixedSizeArray{Int}, FixedSizeVector{Int})
+        for T ∈ (FSA, FSV, FSA{Int}, FSV{Int})
             for iterator ∈ (Iterators.repeated(7), Iterators.cycle(7))
                 @test_throws ArgumentError collect_as(T, iterator)
             end
         end
-        for T ∈ (FixedSizeArray{<:Any,-1}, FixedSizeArray{Int,-1}, FixedSizeArray{Int,3.1})
+        for T ∈ (FSA{<:Any,-1}, FSA{Int,-1}, FSA{Int,3.1})
             iterator = (7:8, (7, 8))
             @test_throws ArgumentError collect_as(T, iterator)
         end
-        for T ∈ (FixedSizeArray{3}, FixedSizeVector{3})
+        for T ∈ (FSA{3}, FSV{3})
             iterator = (7:8, (7, 8))
             @test_throws TypeError collect_as(T, iterator)
         end
@@ -405,7 +411,7 @@ end
             for iterator ∈ (Iter((), 0, 7), Iter((3, 2), 5, 7))
                 E = eltype(iterator)
                 dim_count = length(size(iterator))
-                for T ∈ (FixedSizeArray, FixedSizeArray{E}, FixedSizeArray{<:Any,dim_count}, FixedSizeArray{E,dim_count})
+                for T ∈ (FSA, FSA{E}, FSA{<:Any,dim_count}, FSA{E,dim_count})
                     @test_throws ArgumentError collect_as(T, iterator)
                 end
             end
@@ -422,20 +428,20 @@ end
             (E, dim_count) = abstract_array_params(a)
             af = collect(Float64, iterator)
             @test abstract_array_params(af) == (Float64, dim_count)  # meta
-            @test_throws MethodError collect_as(FixedSizeArray{E,dim_count+1}, iterator)
-            for T ∈ (FixedSizeArray, FixedSizeArray{<:Any,dim_count})
+            @test_throws MethodError collect_as(FSA{E,dim_count+1}, iterator)
+            for T ∈ (FSA, FSA{<:Any,dim_count})
                 fsa = collect_as(T, iterator)
                 @test a == fsa
                 @test first(abstract_array_params(fsa)) <: E
             end
-            for T ∈ (FixedSizeArray{E}, FixedSizeArray{E,dim_count})
-                test_inferred(collect_as, FixedSizeArray{E,dim_count}, (T, iterator))
+            for T ∈ (FSA{E}, FSA{E,dim_count})
+                test_inferred(collect_as, FSA{E,dim_count}, (T, iterator))
                 fsa = collect_as(T, iterator)
                 @test a == fsa
                 @test first(abstract_array_params(fsa)) <: E
             end
-            for T ∈ (FixedSizeArray{Float64}, FixedSizeArray{Float64,dim_count})
-                test_inferred(collect_as, FixedSizeArray{Float64,dim_count}, (T, iterator))
+            for T ∈ (FSA{Float64}, FSA{Float64,dim_count})
+                test_inferred(collect_as, FSA{Float64,dim_count}, (T, iterator))
                 fsa = collect_as(T, iterator)
                 @test af == fsa
                 @test first(abstract_array_params(fsa)) <: Float64
