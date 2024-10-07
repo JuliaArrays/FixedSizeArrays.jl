f62e4012536acc6aea9e282fedcb3dd6fd09f5a9
Mosè Giordano <765740+giordano@users.noreply.github.com>
2024-10-07 20:41:38 +0100

    Define `Base.dataids` method for FixedSizeArrays (#64)
    Co-authored-by: Neven Sajko <s@purelymail.com>

--- src/FixedSizeArrays.jl
+++ src/FixedSizeArrays.jl
@@ -392,4 +392,11 @@ function collect_as(::Type{T}, iterator) where {T<:FixedSizeArray}
     collect_as_fsa_checked(iterator, spec, dim_count, len_stat)::T
 end
 
+if isdefined(Base, :dataids) && (Base.dataids isa Function)
+    # This is an internal, non-public function which is nevertheless needed to
+    # get good performance in some cases (e.g. broadcasting):
+    # <https://github.com/JuliaArrays/FixedSizeArrays.jl/issues/63>.
+    Base.dataids(a::FixedSizeArray) = Base.dataids(a.mem)
+end
+
 end # module FixedSizeArrays
