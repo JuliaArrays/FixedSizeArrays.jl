09f7172f154d518c07726499a6c4e99c34d1e92b
Mosè Giordano <m.giordano@ucl.ac.uk>
2025-02-26 10:18:48 +0100

    Add new `rand!` method to resolve ambiguity

--- src/stdlibs.jl
+++ src/stdlibs.jl
@@ -3,4 +3,8 @@ using Random: Random
 if isdefined(Base, :Memory)
     Random.rand!(rng::Random.AbstractRNG, A::FixedSizeArray{T,N,Memory{T}}, sp::Random.Sampler) where {N,T} =
         Random.rand!(rng, A.mem, sp)
+
+    # Method needed only to resolve ambiguities, we don't care too much about it.
+    Random.rand!(r::Random.MersenneTwister, A::FixedSizeArrayDefault{Float64,N}, I::Random.SamplerTrivial{<:Random.FloatInterval{Float64}}) where {N} =
+        Random.rand!(r, A.mem, I)
 end
