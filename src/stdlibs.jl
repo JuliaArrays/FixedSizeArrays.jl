ba9201be0ee358ce041931d34c0d58cae5bde528
Mosè Giordano <m.giordano@ucl.ac.uk>
2025-02-26 23:08:17 +0100

    Resolve ambiguities for v1.10 too

--- src/stdlibs.jl
+++ src/stdlibs.jl
@@ -3,6 +3,10 @@ using Random: Random
 Random.rand!(rng::Random.AbstractRNG, A::FixedSizeArray{T,N}, sp::Random.Sampler) where {N,T} =
     Random.rand!(rng, A.mem, sp)
 
-# Method needed only to resolve ambiguities, we don't care too much about it.
+# Methods needed only to resolve ambiguities, we don't care too much about them
 Random.rand!(r::Random.MersenneTwister, A::FixedSizeArray{Float64,N}, I::Random.SamplerTrivial{<:Random.FloatInterval{Float64}}) where {N} =
     Random.rand!(r, A.mem, I)
+if VERSION < v"1.11"
+    Random.rand!(::Random._GLOBAL_RNG, A::FixedSizeArray{Float64,N}, I::Random.SamplerTrivial{<:Random.FloatInterval{Float64}}) where {N} =
+        Random.rand!(Random.default_rng(), A.mem, I)
+end
