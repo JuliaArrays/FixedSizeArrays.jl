043faa60f425270750202304bc28767a8d122a59
Mosè Giordano <mose@gnu.org>
2025-06-08 20:38:53 +0100

    Make `rand!(A::FixedSizeArray)` return `A` instead of its parent

--- ext/RandomExt.jl
+++ ext/RandomExt.jl
@@ -3,15 +3,22 @@ module RandomExt
 using Random: Random
 using FixedSizeArrays: FixedSizeArray
 
-Random.rand!(rng::Random.AbstractRNG, A::FixedSizeArray{T,N}, sp::Random.Sampler) where {N,T} =
+function Random.rand!(rng::Random.AbstractRNG, A::FixedSizeArray{T,N}, sp::Random.Sampler) where {N,T}
     Random.rand!(rng, parent(A), sp)
+    return A
+end
 
 # Methods needed only to resolve ambiguities, we don't care too much about them
-Random.rand!(r::Random.MersenneTwister, A::FixedSizeArray{Float64,N}, I::Random.SamplerTrivial{<:Random.FloatInterval{Float64}}) where {N} =
+function Random.rand!(r::Random.MersenneTwister, A::FixedSizeArray{Float64,N}, I::Random.SamplerTrivial{<:Random.FloatInterval{Float64}}) where {N}
     Random.rand!(r, parent(A), I)
+    return A
+end
+
 if VERSION < v"1.11"
-    Random.rand!(::Random._GLOBAL_RNG, A::FixedSizeArray{Float64,N}, I::Random.SamplerTrivial{<:Random.FloatInterval{Float64}}) where {N} =
+    function Random.rand!(::Random._GLOBAL_RNG, A::FixedSizeArray{Float64,N}, I::Random.SamplerTrivial{<:Random.FloatInterval{Float64}}) where {N}
         Random.rand!(Random.default_rng(), parent(A), I)
+        return A
+    end
 end
 
 end # module RandomExt
