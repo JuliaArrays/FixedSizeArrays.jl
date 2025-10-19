e15e11873b16be65529c1b2e695c90693b416502
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-10-19 22:16:01 +0100

    Disambiguate names of extensions (#162)

--- ext/FixedSizeArraysAdaptExt.jl
+++ ext/FixedSizeArraysAdaptExt.jl
@@ -1,8 +1,8 @@
-module AdaptExt
+module FixedSizeArraysAdaptExt
 
 using Adapt
 using FixedSizeArrays: FixedSizeArray
 
 Adapt.adapt_storage(fixed::Type{<:FixedSizeArray}, A) = fixed(A) 
 
-end # module AdaptExt
+end # module FixedSizeArraysAdaptExt
